6bf27164264bd576b3d20866e99131c508ef67b2
gjdhsvsv <2148348843@qq.com>
2026-07-14 14:44:49 +0800

    feat: implement xv6 sysinfo syscall

--- kernel/defs.h
+++ kernel/defs.h
@@ -63,6 +63,7 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
+uint64          freemem(void);
 
 // log.c
 void            initlog(int, struct superblock*);
@@ -93,6 +94,7 @@ int             kill(int);
 struct cpu*     mycpu(void);
 struct cpu*     getmycpu(void);
 struct proc*    myproc();
+uint64          nproc(void);
 void            procinit(void);
 void            scheduler(void) __attribute__((noreturn));
 void            sched(void);
--- kernel/proc.c
+++ kernel/proc.c
@@ -85,6 +85,21 @@ myproc(void) {
   return p;
 }
 
+uint64
+nproc(void)
+{
+  uint64 count = 0;
+  struct proc *p;
+
+  for(p = proc; p < &proc[NPROC]; p++){
+    acquire(&p->lock);
+    if(p->state != UNUSED)
+      count++;
+    release(&p->lock);
+  }
+  return count;
+}
+
 int
 allocpid() {
   int pid;
